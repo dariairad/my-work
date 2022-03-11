46a290fc7e7ec88867fb1ef4ef505758e6ad4577
dariagmit <g00411221@gmit.ie>
2022-03-11 10:32:11 +0000

    code for lab 05

---
+++ lectures-and-labs/week05/lab5.2-summer.py
@@ -0,0 +1,14 @@
+# Lab 5.2
+
+# Create a tuple that stores the months of the year,
+# from that tuple create another tuple with just the summer months (May, June, July),
+# print out the summer months one at a time.
+
+# Author: Daria Sep
+
+months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+summer = months[4:7]
+
+for month in summer:
+    print (month)  
---
+++ lectures-and-labs/week05/lab5.3-queue.py
@@ -0,0 +1,27 @@
+# Lab 5.3
+
+# Create a program that puts 10 random numbers into a queue(list), 
+# the program should then output all the values in the queue, 
+# then take the numbers from the queue one at a time, print it and the current numbers still in the queue. 
+# (the command pop(0) takes the first element out of a list)
+
+# Author: Daria Sep
+
+
+import random
+
+queue = []
+numberOfNumbers = 10
+rangeTo = 100
+
+for n in range (0, numberOfNumbers):
+    queue.append (random.randint (0, rangeTo))
+
+print ('The queue is {}' .format(queue))
+
+while len(queue) != 0: 
+
+    currentNumber = queue.pop(0)
+    print ('Current number is {} and the queue is {}' .format(currentNumber, queue))
+
+print ('The queue is now empty')
